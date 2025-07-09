4dad14d1a6ae9f3c8a51b654fd72da17031b5085
dvlpr_kaushik <103560296+dvlprkaushik@users.noreply.github.com>
2025-07-09 20:43:29 +0530

    ✨ register in routes

--- src/routes/topic.routes.ts
+++ src/routes/topic.routes.ts
@@ -1,10 +1,10 @@
-import { getTopicsByForumId } from "@/controllers/topic.controller.js";
+import { createTopicByForumId, getTopicsByForumId } from "@/controllers/topic.controller.js";
 import { Router } from "express";
 
 
 const topicRouter = Router({ mergeParams: true });
 
-topicRouter.route("/topics").get(getTopicsByForumId);
+topicRouter.route("/topics").get(getTopicsByForumId).post(createTopicByForumId);
 
 
 export { topicRouter };
