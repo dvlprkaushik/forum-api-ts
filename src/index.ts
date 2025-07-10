c1b7647b625f42333397f0e00d76370487834e5c
dvlpr_kaushik <103560296+dvlprkaushik@users.noreply.github.com>
2025-07-10 19:51:15 +0530

    🔧 Minor updates in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,22 @@ import dotenv from "dotenv";
 import { healthCheck } from "./utils/HealthCheck.js";
 import { expressMiddlewares } from "./middlewares/express.middlewares.js";
 import { endpointLogger } from "./middlewares/endpointLogger.middleware.js";
-import { globalErrorMiddleware, notFoundHandler } from "./middlewares/errorHandler.middleware.js";
+import {
+  globalErrorMiddleware,
+  notFoundHandler,
+} from "./middlewares/errorHandler.middleware.js";
 import { forumRouter } from "./routes/forum.routes.js";
 import { listner } from "./listner.js";
 
-dotenv.config({quiet : true});
+dotenv.config({ quiet: true });
 
 const app = express();
 expressMiddlewares(app, express);
-app.use(endpointLogger);
-
-// HealthCheck
-healthCheck(app);
+if (process.env.NODE_ENV === "development") {
+  app.use(endpointLogger);
+  healthCheck(app);
+  // HealthCheck
+}
 
 // Routes
 app.use("/api", forumRouter);
@@ -22,7 +26,6 @@ app.use("/api", forumRouter);
 // global-middlewares
 app.use(notFoundHandler);
 app.use(globalErrorMiddleware);
-
 
 // listner
 listner(app);
